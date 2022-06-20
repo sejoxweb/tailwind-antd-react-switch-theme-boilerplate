5a2daf2cc9ce73f56c319bc737b71df300c5d28f
jafar1405 <jafar1405@gmail.com>
2022-06-20 10:50:30 +0530

    initial Commit

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+require("dotenv/config");
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in project
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+  plugins: [
+    tailwindcss("./tailwind.js"),
+    require("autoprefixer"),
+    ...(process.env.REACT_APP_NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./themes/tailwind.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const themes = {
+  dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
+  light: `${process.env.PUBLIC_URL}/light-theme.css`,
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeSwitcherProvider themeMap={themes} defaultTheme="light">
+      <App />
+    </ThemeSwitcherProvider>
   </React.StrictMode>
 );
 
